e8c8b04fb0d59a98775e17f0ed00c8457c9e104c
KSHcodered <seunghwan@codered.cool>
2021-03-10 17:09:55 +0900

    up

---
+++ 20210308_UI/gabangkkeon_1/lib/TextWriting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class WritingPage extends StatefulWidget {
+  @override
+  _WritingPageState createState() => _WritingPageState();
+}
+
+class _WritingPageState extends State<WritingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
